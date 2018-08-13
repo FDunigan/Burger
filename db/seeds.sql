f6d6efba3ce9c17942b08284bdd816b15bdc7c06
Doogle <fdunigan11@gmail.com>
2018-08-13 14:52:53 -0400

    initial setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', false);
