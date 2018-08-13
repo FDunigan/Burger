0cf501a122b5580b6d4acb5b7de9cbc72330a7ba
Doogle <fdunigan11@gmail.com>
2018-08-13 16:58:29 -0400

    updated burger, orm, and server JS files

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,31 @@
-var connection = require("./connection.js");
+var connection = require("../config/connection.js");
 
 var orm = {
-    selectAll: function() {
-
+    selectAll: function(cb) {
+        connection.query("SELECT * FROM burgers", function() {
+            if (err) throw err;
+            console.log(result);
+            cb(result);
+        })
     },
-    insertOne: function () {
-
-    },
-    updateOne: function() {
-
+    insertOne: function(burger_name, cb) {
+        connection.query("INSERT INTO burgers SET ?", {
+            burger_name: burger_name,
+            devoured: false,
+        }, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            cb(result);
+        });
+    }, 
+    updateOne: function(burgerID, cb) {
+        connection.query("UPDATE burgers SET ? WHERE ?", [{devoured: true}, {id: burgerID}], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            cb(result);
+        });
     }
 };
 
-
+// export the ORM object
 module.exports = orm;
