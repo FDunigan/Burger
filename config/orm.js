6297cf2befc06fe0f4a0f4353d3231dd40b99a94
Doogle <fdunigan11@gmail.com>
2018-08-14 14:21:26 -0400

    changes to the page layout and cleaning up config, controllers, and models files

--- config/orm.js
+++ config/orm.js
@@ -2,24 +2,24 @@ var connection = require("../config/connection.js");
 
 var orm = {
     selectAll: function(cb) {
-        connection.query("SELECT * FROM burgers", function() {
+        var queryString = "SELECT * FROM burgers";
+        connection.query(queryString, function(err, result) {
             if (err) throw err;
             console.log(result);
             cb(result);
         })
     },
     insertOne: function(burger_name, cb) {
-        connection.query("INSERT INTO burgers SET ?", {
-            burger_name: burger_name,
-            devoured: false,
-        }, function(err, result) {
+        var queryString = "INSERT INTO burgers SET ?";
+        connection.query(queryString, [{burger_name: burger_name, devoured: false}], function(err, result) {
             if (err) throw err;
             console.log(result);
             cb(result);
         });
     }, 
     updateOne: function(burgerID, cb) {
-        connection.query("UPDATE burgers SET ? WHERE ?", [{devoured: true}, {id: burgerID}], function(err, result) {
+        var queryString = "UPDATE burgers SET ? WHERE ?";
+        connection.query(queryString, [{devoured: true}, {id: burgerID}], function(err, result) {
             if (err) throw err;
             console.log(result);
             cb(result);
@@ -27,5 +27,4 @@ var orm = {
     }
 };
 
-// export the ORM object
 module.exports = orm;
